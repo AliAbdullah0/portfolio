25213767d3d32a59d165faf6e3122291b1d646d3
AliAbdullah0 <aliabdullah10nov2006@gmail.com>
2024-12-03 22:40:08 +0500

    faf

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,4 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/',
+  base: '/portfolio/', // Must match repository name
 });
